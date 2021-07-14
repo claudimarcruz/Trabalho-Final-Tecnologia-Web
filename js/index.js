3c1f1bbcd0813e9826a302e90ef401ff0d9e53c0
claudimarcruz <claudimarcruz3@gmail.com>
2021-07-14 19:45:31 -0300

    Trabalho final

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var linhas3 = document.querySelector('.linhas3');
+
+linhas3.addEventListener("click",function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
